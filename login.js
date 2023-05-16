df294adefe3d63d41d37621f251f5731515402c2
aswanthanoop <aswanthanoop42@gmail.com>
2023-05-16 11:03:59 +0530

    Added user output form

---
+++ login.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDLMdDwa_wKXK9nqJ_Vc0P7IzUg5uAqzes",
+    authDomain: "login-for-hivemind.firebaseapp.com",
+    databaseURL: "https://login-for-hivemind-default-rtdb.firebaseio.com",
+    projectId: "login-for-hivemind",
+    storageBucket: "login-for-hivemind.appspot.com",
+    messagingSenderId: "674861732608",
+    appId: "1:674861732608:web:57baa4957ff22ed2ea99e5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
